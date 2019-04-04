f5259ab621a8eae92aadc8b4596bc0a182f9704b
Scovia Nnamiggo <2305senk@gmail.com>
2019-04-04 05:01:32 +0300

    adding functionality to login page

--- js/login.js
+++ js/login.js
@@ -17,6 +17,7 @@ alert(data);
 }else if(data=='Email or Password is wrong...!!!!'){
 $('input[type="text"],input[type="password"]').css({"border":"2px solid red","box-shadow":"0 0 3px red"});
 alert(data);
+// try again or signup
 } else if(data=='Successfully Logged in...'){
 $("form")[0].reset();
 $('input[type="text"],input[type="password"]').css({"border":"2px solid #00F5FF","box-shadow":"0 0 5px #00F5FF"});
