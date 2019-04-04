c2aa6944281695fca25654a435d39ead239d6dd5
Scovia Nnamiggo <2305senk@gmail.com>
2019-04-04 08:01:38 +0300

    adding functionality to login page

--- js/login.js
+++ js/login.js
@@ -3,26 +3,33 @@ $("#login").click(function(){
 var user_name = $("#uname").val();
 var password = $("#password").val();
 // Checking for blank fields.
-if( email =='' || password ==''){
+if( user_name =='' || password ==''){
 $('input[type="text"],input[type="password"]').css("border","2px solid red");
 $('input[type="text"],input[type="password"]').css("box-shadow","0 0 3px red");
 alert("Please fill all fields...!!!!!!");
-}else {
+}
+else {
 $.post("login.php",{ user_name1: user_name, password1:password},
 function(data) {
-if(data=='Invalid Email.......') {
+if(data=='Invalid Username.......') {
 $('input[type="text"]').css({"border":"2px solid red","box-shadow":"0 0 3px red"});
 $('input[type="password"]').css({"border":"2px solid #00F5FF","box-shadow":"0 0 5px #00F5FF"});
 alert(data);
-}else if(data=='Email or Password is wrong...!!!!'){
+}
+else if(data=='Username or Password is wrong...!!!!'){
 $('input[type="text"],input[type="password"]').css({"border":"2px solid red","box-shadow":"0 0 3px red"});
 alert(data);
 // try again or signup
-} else if(data=='Successfully Logged in...'){
+}
+else if(data=='Successfully Logged in...'){
 $("form")[0].reset();
 $('input[type="text"],input[type="password"]').css({"border":"2px solid #00F5FF","box-shadow":"0 0 5px #00F5FF"});
 alert(data);
-} else{
-alert(data);
+// } else{
+// alert(data);}
 }
+}
+);
+}
+});
 });
--- login.php
+++ login.php
@@ -1,13 +1,18 @@
 <?php
-$connection = mysql_connect("localhost", "root", "stressfree"); // Establishing Connection with Server..
-$db = mysql_select_db("STRESS_FREE_EVENTS", $connection); // Selecting Database
-//Fetching Values from URL
-$name2=$_POST['name1'];
-$password2=$_POST['password1'];
-$id2=$_POST['id1'];
-$status2=$_POST['status1'];
-//Insert query
-$query = mysql_query("insert into LOGIN(Status, User_name, Password, ID) values ('$status2', '$name2', '$password2','$id2')");
-echo "Login successful";
-mysql_close($connection); // Connection Closed
+$connection = mysql_connect("localhost", "root", "stressfree"); // Establishing connection with server..
+$db = mysql_select_db("STRESS_FREE_EVENTS", $connection); // Selecting Database.
+$user_name=$_POST['user_name1']; // Fetching Values from URL.
+$password= sha1($_POST['password1']); // Password Encryption, If you like you can also leave sha1.
+
+// Matching user input email and password with stored email and password in database.
+$result = mysql_query("SELECT * FROM LOGIN WHERE User_name='$user_name' AND password='$password'");
+$data = mysql_num_rows($result);
+if($data==1){
+echo "Successfully Logged in...";
+}else{
+echo "Email or Password is wrong...!!!!";
+//sugest sign up or try anagain
+}
+
+mysql_close ($connection); // Connection Closed.
 ?>
